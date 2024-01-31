4e193b5804af889ff502ebda380b80b620423aeb
Andrew Plutto <pluttan@ya.ru>
2024-01-31 13:58:25 +0300

    gitsings

--- lua/plugins/code/git.lua
+++ lua/plugins/code/git.lua
@@ -9,4 +9,11 @@ return {
             vim.keymap.set("n", '<M-g>', ':LazyGit<CR>', {})
         end
     },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function ()
+            require('gitsigns').setup()
+            vim.cmd(":Gitsigns toggle_current_line_blame")
+        end
+    },
 }
