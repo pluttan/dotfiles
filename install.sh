56c2389bc0f2e09672f9589eae024e3bc08b3d11
pluttan <pluttan@ya.ru>
2024-02-10 12:53:18 +0000

    debian&ubuntu install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 # Update zsh plugins and link .zshrc
 rm -rf ~/.dotfiles/zsh/plugins
-git clone https://github.com/dracula/zsh-syntax-highlighting.git ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting-dracula
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/zsh/plugins/zsh-autosuggestions
+#git clone https://github.com/dracula/zsh-syntax-highlighting.git ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting-dracula
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/zsh/plugins/zsh-autosuggestions
+rm ~/.zshrc
 stow -d ~/.dotfiles/zsh/stow~ -t ~/ .
 
-stow -d ~/.dotfiles/tmux/stow~ -t ~/ .
+stow --adopt -d ~/.dotfiles/tmux/stow~ -t ~/ .
 
+rm -rf ~/.config/nvim
 mkdir ~/.config/nvim
-stow -d ~/.dotfiles/nvim -t ~/.config/nvim .
+stow --adopt -d ~/.dotfiles/nvim -t ~/.config/nvim .
 
-stow -d ~/.dotfiles/dos -t ~/ .
+stow --adopt -d ~/.dotfiles/dos -t ~/ .
 
 git config --global core.excludesfile ~/.dotfiles/git/.gitignore
 
-rm ~/Library/Application\ Support/lazygit/config.yml
-stow -d ~/.dotfiles/git/lazygit -t ~/Library/Application\ Support/lazygit .
+rm ~/.config/lazygit/config.yml
+stow --adopt -d ~/.dotfiles/git/lazygit -t ~/.config/lazygit .
