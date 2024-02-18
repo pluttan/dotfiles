4f2febfeb169eb1459428b10791899329ed4fe82
pluttan <pluttan@ya.ru>
2024-02-18 08:14:36 +0000

    remove block arrows in insert mode

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -9,11 +9,11 @@ vim.cmd("nnoremap <Left>  :echoe 'Use h'<CR>")
 vim.cmd("nnoremap <Right> :echoe 'Use l'<CR>")
 vim.cmd("nnoremap <Up>    :echoe 'Use k'<CR>")
 vim.cmd("nnoremap <Down>  :echoe 'Use j'<CR>")
-vim.cmd("inoremap <Left>  <ESC>:echoe 'Use h'<CR>")
-vim.cmd("inoremap <Right> <ESC>:echoe 'Use l'<CR>")
-vim.cmd("inoremap <Up>    <ESC>:echoe 'Use k'<CR>")
-vim.cmd("inoremap <Down>  <ESC>:echoe 'Use j'<CR>")
-
+-- vim.cmd("inoremap <Left>  <ESC>:echoe 'Use h'<CR>")
+-- vim.cmd("inoremap <Right> <ESC>:echoe 'Use l'<CR>")
+-- vim.cmd("inoremap <Up>    <ESC>:echoe 'Use k'<CR>")
+-- vim.cmd("inoremap <Down>  <ESC>:echoe 'Use j'<CR>")
+--
 vim.g.mapleader = " "
 
 -- Opt settings
