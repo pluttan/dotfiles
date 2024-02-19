89c64077c26ce86bf0a73aefa28491853f2182c2
pluttan <pluttan@ya.ru>
2024-02-19 19:22:25 +0000

    lg

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,12 +4,12 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set cmdheight=0")
 vim.cmd("set breakindent")
-
-vim.cmd("nnoremap <Left>  :echoe 'Use h'<CR>")
-vim.cmd("nnoremap <Right> :echoe 'Use l'<CR>")
-vim.cmd("nnoremap <Up>    :echoe 'Use k'<CR>")
-vim.cmd("nnoremap <Down>  :echoe 'Use j'<CR>")
--- vim.cmd("inoremap <Left>  <ESC>:echoe 'Use h'<CR>")
+--
+-- vim.cmd("nnoremap <Left>  :echoe 'Use h'<CR>")
+-- vim.cmd("nnoremap <Right> :echoe 'Use l'<CR>")
+-- vim.cmd("nnoremap <Up>    :echoe 'Use k'<CR>")
+-- vim.cmd("nnoremap <Down>  :echoe 'Use j'<CR>")
+-- -- vim.cmd("inoremap <Left>  <ESC>:echoe 'Use h'<CR>")
 -- vim.cmd("inoremap <Right> <ESC>:echoe 'Use l'<CR>")
 -- vim.cmd("inoremap <Up>    <ESC>:echoe 'Use k'<CR>")
 -- vim.cmd("inoremap <Down>  <ESC>:echoe 'Use j'<CR>")
@@ -17,7 +17,7 @@ vim.cmd("nnoremap <Down>  :echoe 'Use j'<CR>")
 vim.g.mapleader = " "
 
 -- Opt settings
-vim.opt.mouse = ''
+vim.opt.mouse = 'a'
 vim.opt.encoding = 'utf-8'
 vim.opt.swapfile = false
 vim.opt.smartindent = true
