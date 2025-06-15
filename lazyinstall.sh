d5be004c0ec695eca9aadfcf57510882c72a8c0f
Andrew Plutto <pluttan@ya.ru>
2025-06-15 07:50:39 +0300

    lazyinstall

--- lazyinstall.sh
+++ lazyinstall.sh
@@ -1,5 +1,5 @@
 wget https://raw.githubusercontent.com/pluttan/dotfiles/refs/heads/main/installnew.sh
-wget https://raw.githubusercontent.com/pluttan/volumes/main/volumes.sh
+wget https://raw.githubusercontent.com/pluttan/volumes/refs/heads/main/volumes.sh
 chmod +x volumes.sh
 sh -c "$(./volumes.sh)" -- installnew.sh
 rm installnew.sh volumes.sh
